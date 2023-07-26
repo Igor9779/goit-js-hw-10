1bc9b3475b883409534a157fdb7a19bb127b613e
Igor9779 <darthhoit97@gmail.com>
2023-07-26 19:25:04 +0300

    prepare-01

--- src/cat-api.js
+++ src/cat-api.js
@@ -6,22 +6,22 @@ const BASE_URL = 'https://api.thecatapi.com/v1';
 
 export function fetchBreeds() {
     return axios.get(`${BASE_URL}/breeds`)
-    .then((response) => {
-        return response.data;
-    })
-    .catch((error) => {
-        console.error('Error fetching:', error);
-    return [];
-    });
+        .then((response) => {
+            return response.data;
+        })
+        .catch((error) => {
+            console.error('Error fetching breeds:', error);
+            throw error;
+        });
 };
 
 export function fetchCatByBreed(breedId) {
-    return axios.get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
-    .then((response) => {
-    return response.data;
-    })
-    .catch((error) => {
-        console.error('Error fetching:', error);
-    return null;
-    });
+    return axios.get(`${BASE_URL}/images/search?breed_ids=${breedId}4`)
+        .then((response) => {
+            return response.data;
+        })
+        .catch((error) => {
+            console.error('Error fetching cat data:', error);
+            throw error;
+        });
 };
--- src/index.js
+++ src/index.js
@@ -1,50 +1,65 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
-
 const select = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
+const catInfo = document.querySelector('.cat-info');
 
 select.addEventListener('change', onSearch);
+
+function showLoader() {
+    loader.style.display = 'block';
+    select.disabled = true; 
+    error.classList.remove('show-error');
+    catInfo.style.display = 'none';
+}
+
+function hideLoader() {
+    loader.style.display = 'none';
+    select.disabled = false;
+}
+
+function showError() {
+    error.classList.add('show-error');
+}
+
+function hideError() {
+    error.classList.remove('show-error');
+}
 
 function onSearch(e) {
     const selectBreedId = e.target.value;
 
-    loader.style.display = 'block';
-    error.classList.add('hide');
+    showLoader();
+    hideError();
+    catInfo.style.display = 'none';
 
     fetchCatByBreed(selectBreedId)
-    .then((catData) => {
+        .then((catData) => {
+            hideLoader();
 
-        loader.style.display = 'none';
+            if (catData && catData[0] && catData[0].url) {
+                catInfo.style.display = 'block';
 
-    if (catData) {
-        const catInfo = document.querySelector('.cat-info');
-
-        catInfo.style.display = 'block';
-
-        catInfo.innerHTML = `
-            <img src='${catData[0].url}' alt='Cat Image'>
-            <p>Breed Name:<span class='breed-name'>${catData[0].breeds[0].name}</span></p>
-            <p>Description:<span class='description'>${catData[0].breeds[0].description}</span></p>
-            <p>Temperament:<span class='temperament'>${catData[0].breeds[0].temperament}</span></p>
-        `;
-    } else {
-        error.classList.remove('hide');
-    };
-    })
-    .catch((error) => {
-        console.error('Error fetching:', error);
-
-        error.classList.remove('hide');
-        loader.style.display = 'none';
-    });
+                catInfo.innerHTML = `
+                    <img src='${catData[0].url}' alt='Cat Image'>
+                    <p>Breed Name:<span class='breed-name'>${catData[0].breeds[0].name}</span></p>
+                    <p>Description:<span class='description'>${catData[0].breeds[0].description}</span></p>
+                    <p>Temperament:<span class='temperament'>${catData[0].breeds[0].temperament}</span></p>
+                `;
+            } else {
+                showError();
+            }
+        })
+        .catch((error) => {
+            console.error('Error fetching cat data:', error);
+            hideLoader();
+            showError();
+        });
 };
 
 fetchBreeds()
     .then((breeds) => {
-        loader.style.display = 'none';
-
         breeds.forEach((breed) => {
             const option = document.createElement('option');
             option.value = breed.id;
@@ -53,8 +68,7 @@ fetchBreeds()
         });
     })
     .catch((error) => {
-        console.error('Error fetching:', error);
+        console.error('Error fetching breeds:', error);
+        showError();
+    });
 
-        error.classList.remove('hide');
-        loader.style.display = 'none';
-});
