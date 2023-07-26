37d2ef55bf28528211d49b569dc28dd574cc9778
Igor9779 <darthhoit97@gmail.com>
2023-07-26 22:01:18 +0300

    prepare-03

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,27 +1,22 @@
-import axios from 'axios';
-
-axios.defaults.headers.common["x-api-key"] = 'live_faod7F7iwdTeHxKxxNpPlufEV4OGiaHx2txyhON8I5XLJGky5rsULrQHLkTNt8ps';
-
+const API_KEY = 'live_faod7F7iwdTeHxKxxNpPlufEV4OGiaHx2txyhON8I5XLJGky5rsULrQHLkTNt8ps';
 const BASE_URL = 'https://api.thecatapi.com/v1';
 
 export function fetchBreeds() {
-    return axios.get(`${BASE_URL}/breeds`)
-        .then((response) => {
-            return response.data;
-        })
-        .catch((error) => {
-            console.error('Error fetching breeds:', error);
-            throw error;
-        });
+    return fetch(`${BASE_URL}/breeds?api_key=${API_KEY}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });       
 };
 
 export function fetchCatByBreed(breedId) {
-    return axios.get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
-        .then((response) => {
-            return response.data;
-        })
-        .catch((error) => {
-            console.error('Error fetching cat data:', error);
-            throw error;
-        });
+    return fetch(`${BASE_URL}/images/search?api_key=${API_KEY}&breed_ids=${breedId}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        });  
 };
